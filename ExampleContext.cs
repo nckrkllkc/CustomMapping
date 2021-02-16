842fad033ee240dd874f7d97e278c5733bf8594f
Naci Karakullukçu <n.karakullukcu@hotmail.com>
2021-02-16 11:07:09 +0300

    Add project files.

---
+++ ExampleContext.cs
@@ -0,0 +1,32 @@
+﻿using CustomMapping.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomMapping
+{
+    public class ExampleContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Connection string
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb; Database=DbName; Trusted_Connection=true");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Veritabanındaki tablolarla proje içerisindeki tabloların map işlemi
+            //default database schema
+            modelBuilder.HasDefaultSchema("dbo");
+            modelBuilder.Entity<Product>().ToTable("Products");
+            //modelBuilder.Entity<Product>().ToTable("Products","dbo");
+
+            modelBuilder.Entity<Product>().Property(p => p.Id).HasColumnName("Id");
+            modelBuilder.Entity<Product>().Property(p => p.ProductName).HasColumnName("Name");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
